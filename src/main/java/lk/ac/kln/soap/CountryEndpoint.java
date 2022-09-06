28f97e05d3a05dd7f8e68b9371f29cea9d8a1946
Sithum <sithum.dashantha@gmail.com>
2022-09-06 18:26:21 +0530

    Entry Point

---
+++ src/main/java/lk/ac/kln/soap/CountryEndpoint.java
@@ -0,0 +1,29 @@
+package lk.ac.kln.soap;
+
+import lk.ac.kln.gen.GetCountryRequest;
+import lk.ac.kln.gen.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URL = "http://www.kln.ac.lk/gen";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URL, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountryByName(request.getName()));
+        return response;
+    }
+}
